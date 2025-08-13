d7b5fcc1aba90e1ca4dfc38b186247d355c521eb
Jahongir1221 <jahongirqambarov@gmail.com>
2025-08-13 14:47:06 +0500

    Create skript.py

---
+++ lesson-1/homework/skript.py
@@ -0,0 +1,6 @@
+Homework:
+
+Given a side of square. Find its perimeter and area.
+Given diameter of circle. Find its length.
+Given two numbers a and b. Find their mean.
+Given two numbers a and b. Find their sum, product and square of each number.
